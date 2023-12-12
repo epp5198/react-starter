d0c71c543f65e1c5d114ce651748a9685ff01f76
egan <epp5198@psu.edu>
2023-12-12 17:04:28 -0500

    pushing working page??

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
-import { useState } from 'react';
+import React, { useState } from 'react';
 import produce from 'immer';
+import { Link } from 'react-router-dom';
 
 const initialList = [
     { id: 0, title: 'Great Barrier Reef', seen: false },
@@ -59,84 +60,21 @@ function ItemList({ artworks, onToggle }) {
     );
 }
 
-export function Form() {
-    const [answer, setAnswer] = useState('');
-    const [error, setError] = useState(null);
-    const [status, setStatus] = useState('typing');
+// Remove the unnecessary submitForm function
 
-    if (status === 'success') {
-        return <h1>That's right!</h1>;
-    }
+const Navbar = () => (
+    <nav>
+        <ul>
+            <li>
+                <Link to="/">Home</Link>
+            </li>
+            <li>
+                <Link to="/assets">Photographs</Link>
+            </li>
+        </ul>
+    </nav>
+);
 
-    async function handleSubmit(e) {
-        e.preventDefault();
-        setStatus('submitting');
-        try {
-            await submitForm(answer);
-            setStatus('success');
-        } catch (err) {
-            setStatus('typing');
-            setError(err);
-        }
-    }
-
-    function handleTextareaChange(e) {
-        setAnswer(e.target.value);
-    }
-
-    return (
-        <>
-            <h2>test quiz</h2>
-            <p>What is 5+5</p>
-            <form onSubmit={handleSubmit}>
-        <textarea
-            value={answer}
-            onChange={handleTextareaChange}
-            disabled={status === 'submitting'}
-        />
-                <br />
-                <button
-                    disabled={answer.length === 0 || status === 'submitting'}
-                >
-                    Submit
-                </button>
-                {error !== null && (
-                    <p className="Error">{error.message}</p>
-                )}
-            </form>
-        </>
-    );
-}
-
-function submitForm(answer) {
-    return new Promise((resolve, reject) => {
-        setTimeout(() => {
-            let shouldError = answer.toLowerCase() !== '10';
-            if (shouldError) {
-                reject(
-                    new Error('Sorry, wrong answer. Try again')
-                );
-            } else {
-                resolve();
-            }
-        }, 1500);
-    });
-}
-import { Link } from 'react-router-dom'; // Assuming you are using React Router for navigation
-
-const Navbar = () => {
-    return (
-        <nav>
-            <ul>
-                <li>
-                    <Link to="/">Home</Link>
-                </li>
-                <li>
-                    <Link to="/assets">Photographs</Link>
-                </li>
-            </ul>
-        </nav>
-    );
-};
+export { Navbar }; // Add export statement for Navbar
 
 
